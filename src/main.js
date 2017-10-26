b06446367cec988141f47873f0de5a99444c7835
Luis <luismelo.mg@gmail.com>
2017-10-26 21:03:31 -0200

    Criacao de rotas

--- src/main.js
+++ src/main.js
@@ -2,13 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
 import VueMaterial from 'vue-material';
+import VueRouter from 'vue-router';
+import {routes} from './routes';
 
 // registrando o módulo/plugin no global view object
 Vue.use(VueResource);
 Vue.use(VueMaterial);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes,
+    mode: 'history' /* o servidor deve retornar por padrao sem a index.html independente da url */
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 });
 
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+/**
+ * Created by luis on 10/26/17.
+ */
+import Home from './components/home/Home.vue';
+import Cadastro from './components/cadastro/Cadastro.vue';
+
+export const routes = [
+
+    {path: '', component: Home},
+    {path: '/cadastro', component: Cadastro}
+
+];
