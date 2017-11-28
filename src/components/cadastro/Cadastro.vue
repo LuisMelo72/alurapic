a40c6adb488ba4c3d280cd326ca7a87d66eeb067
Luis <luismelo.mg@gmail.com>
2017-11-28 20:47:47 -0200

    Directive v-model para two way databinding

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -9,25 +9,29 @@
                 <label for="titulo">TÍTULO</label>
                 <input id="titulo"
                        autocomplete="off"
-                       @input="foto.titulo = $event.target.value"
-                       :value="foto.titulo" />
+                       v-model.lazy="foto.titulo" />
+
+                <!--
+                Sem o uso do v-model
+                @input="foto.titulo = $event.target.value"
+                :value="foto.titulo"
+                -->
+
             </div>
 
             <div class="controle">
                 <label for="url">URL</label>
                 <input id="url"
                        autocomplete="off"
-                       @input="foto.url = $event.target.value"
-                       :value="foto.url" />
-                <imagem-responsiva/>
+                       v-model.lazy="foto.url" />
+                <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo" />
             </div>
 
             <div class="controle">
                 <label for="descricao">DESCRIÇÃO</label>
                 <textarea id="descricao"
                           autocomplete="off"
-                          @input="foto.descricao = $event.target.value"
-                          :value="foto.descricao">
+                          v-model="foto.descricao">
                 </textarea>
             </div>
 
